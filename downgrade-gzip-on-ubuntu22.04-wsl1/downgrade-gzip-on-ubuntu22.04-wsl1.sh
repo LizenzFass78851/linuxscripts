828244ef94db2f2b38e6fdee52802c7adf86d61e
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2022-08-02 09:35:03 +0200

    Update downgrade-gzip-on-ubuntu22.04-wsl1.sh

--- downgrade-gzip-on-ubuntu22.04-wsl1/downgrade-gzip-on-ubuntu22.04-wsl1.sh
+++ downgrade-gzip-on-ubuntu22.04-wsl1/downgrade-gzip-on-ubuntu22.04-wsl1.sh
@@ -3,7 +3,7 @@
 gzfile=gzip_1.10-0ubuntu4.1_amd64.deb
 
 wget https://launchpad.net/ubuntu/+archive/primary/+files/$gzfile
-apt install -y --allow-downgrades ./$gzfile
+apt install -y --allow-downgrades --allow-change-held-packages ./$gzfile
 echo gzip hold | dpkg --set-selections
 rm $gzfile
 
