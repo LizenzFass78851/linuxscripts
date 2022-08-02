28894b6523fcc45116a803e7a241b47062157b4f
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2022-08-02 09:08:41 +0200

    Add files via upload

---
+++ downgrade-gzip-on-ubuntu22.04-wsl1/downgrade-gzip-on-ubuntu22.04-wsl1.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+gzfile=gzip_1.10-0ubuntu4.1_amd64.deb
+
+wget https://launchpad.net/ubuntu/+archive/primary/+files/$gzfile
+apt install -y ./$gzfile
+echo gzip hold | dpkg --set-selections
+rm $gzfile
+
