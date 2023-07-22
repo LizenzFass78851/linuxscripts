bd1d2b3527586d287fc6e6c324255dce9d86443d
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-07-22 09:19:35 +0200

    Update linux apps install script.sh

--- install-apps-over-the-script/linux apps install script.sh
+++ install-apps-over-the-script/linux apps install script.sh
@@ -4,15 +4,15 @@
 # config Links, Apps and Hostname
 
 LINKS="https://download.anydesk.com/linux/anydesk_6.2.1-1_amd64.deb
-https://dn3.freedownloadmanager.org/6/latest/freedownloadmanager.deb
+https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb
 https://dl.google.com/dl/linux/direct/google-earth-pro-stable_7.3.6_amd64.deb
 https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-https://download.virtualbox.org/virtualbox/6.1.44/virtualbox-6.1_6.1.44-156814~Ubuntu~jammy_amd64.deb
-https://download.virtualbox.org/virtualbox/6.1.44/Oracle_VM_VirtualBox_Extension_Pack-6.1.44-156814.vbox-extpack
+https://download.virtualbox.org/virtualbox/6.1.46/virtualbox-6.1_6.1.46-158378~Ubuntu~jammy_amd64.deb
+https://download.virtualbox.org/virtualbox/6.1.46/Oracle_VM_VirtualBox_Extension_Pack-6.1.46.vbox-extpack
 https://repo.steampowered.com/steam/archive/stable/steam_latest.deb
-https://dl.discordapp.net/apps/linux/0.0.24/discord-0.0.24.deb
+https://dl.discordapp.net/apps/linux/0.0.28/discord-0.0.28.deb
 https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-https://github.com/shiftkey/desktop/releases/download/release-3.1.1-linux1/GitHubDesktop-linux-3.1.1-linux1.deb"
+https://github.com/shiftkey/desktop/releases/download/release-3.2.5-linux1/GitHubDesktop-linux-3.2.5-linux1.deb"
 
 NEEDEDAPPS="snapd
 tilix"
