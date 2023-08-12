e05706ed45a82eda332f25ae2ae0b9e6cece5efd
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-08-12 18:42:10 +0200

    Delete downgrade-gzip-on-ubuntu22.04-wsl1 directory

--- downgrade-gzip-on-ubuntu22.04-wsl1/downgrade-gzip-on-ubuntu22.04-wsl1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-gzfile=gzip_1.10-0ubuntu4.1_amd64.deb
-
-wget https://launchpad.net/ubuntu/+archive/primary/+files/$gzfile
-apt install -y --allow-downgrades --allow-change-held-packages ./$gzfile
-echo gzip hold | dpkg --set-selections
-rm $gzfile
-
