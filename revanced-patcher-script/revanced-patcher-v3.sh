fa674ed383e10e89c14fd0a03518c96bf5b1168f
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-08-27 15:43:23 +0200

    Add files via upload

---
+++ revanced-patcher-script/revanced-patcher-v3.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+# youtube
+## patch apk
+java \
+  -jar $(ls ./revanced-cli*.jar) patch \
+  --patch-bundle $(ls ./revanced-patches*.jar) \
+  --exclude swipe-controls \
+  --exclude hide-autoplay-button \
+  --exclude always-autorepeat \
+  --exclude downloads \
+  --out youtube_revanced.apk \
+  --merge $(ls ./revanced-integrations*.apk) \
+  "$(ls ./com.google.android.youtube*.apk)"
+
+
+## sign apk
+java \
+  -jar apksigner.jar sign \
+  --ks "revanced-self-build.keystore" \
+  --ks-pass pass:$(cat ./revanced-self-build.password.txt) \
+  --out "youtube_revanced_signed.apk" \
+  "youtube_revanced.apk"
+
+
+
+# twitch
+## patch apk
+java \
+  -jar $(ls ./revanced-cli*.jar) patch \
+  --patch-bundle $(ls ./revanced-patches*.jar) \
+  --include change-package-name \
+  --exclude show-deleted-messages \
+  --out twitch_revanced.apk \
+  --merge $(ls ./revanced-integrations*.apk) \
+  "$(ls ./tv.twitch.android.app*.apk)"
+
+
+## sign apk
+java \
+  -jar apksigner.jar sign \
+  --ks "revanced-self-build.keystore" \
+  --ks-pass pass:$(cat ./revanced-self-build.password.txt) \
+  --out "twitch_revanced_signed.apk" \
+  "twitch_revanced.apk"
