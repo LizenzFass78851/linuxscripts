189d4c82997d84ec38d5824b31c39915219c2e0a
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2026-08-20 09:37:03 +0200

    refactor(buildgluonscript): Revise Dockerfile

--- buildgluonscript/buildgluonscript (separate targets edit).sh
+++ buildgluonscript/buildgluonscript (separate targets edit).sh
@@ -1,23 +1,58 @@
 #!/bin/bash
 
-# the script carries out the workflow to build gluon with the ffc site config
-# All targets with all cpu cores are used when building and broken equals 1
+# Build all targets as of the defined version from gluon, also broken and use all cpu cores (broken can be set in the script)
+# If there were changes to the targets during gluon, this can also be set in the script
 
 SCRIPT_GLUON_REPO="https://github.com/freifunk-gluon/gluon.git"
 SCRIPT_GLUON_BRANCH="v2025.1.x"
+SCRIPT_GLUON_FOLDER="gluon"
 
 SCRIPT_GLUON_SITE_REPO="https://github.com/FreifunkVogtland/site-ffv.git"
 #SCRIPT_GLUON_SITE_REPO="https://github.com/FreifunkChemnitz/site-ffc.git"
 
-git clone --branch $SCRIPT_GLUON_BRANCH $SCRIPT_GLUON_REPO
-cd gluon
+# for Gluon v2025.1.x
+# The targets originate from Gluon v2025.1.1
+TARGETS="armsr-armv7
+armsr-armv8
+ath79-generic
+ath79-mikrotik
+ath79-nand
+bcm27xx-bcm2708
+bcm27xx-bcm2709
+bcm27xx-bcm2710
+bcm27xx-bcm2711
+ipq40xx-chromium
+ipq40xx-generic
+ipq40xx-mikrotik
+ipq806x-generic
+kirkwood-generic
+lantiq-xrx200
+lantiq-xrx200_legacy
+lantiq-xway
+mediatek-filogic
+mediatek-mt7622
+mpc85xx-p1010
+mpc85xx-p1020
+mvebu-cortexa53
+mvebu-cortexa9
+qualcommax-ipq807x
+ramips-mt7620
+ramips-mt7621
+ramips-mt76x8
+rockchip-armv8
+sunxi-cortexa7
+x86-generic
+x86-geode
+x86-legacy
+x86-64"
+
+git clone --branch $SCRIPT_GLUON_BRANCH $SCRIPT_GLUON_REPO $SCRIPT_GLUON_FOLDER
+cd $SCRIPT_GLUON_FOLDER
 git clone $SCRIPT_GLUON_SITE_REPO site
 make update
 
 export DEFAULT_GLUON_RELEASE="b$(date '+%Y%m%d')"
 export BROKEN=1
-
-TARGETS="$(make list-targets)"
 
 for STEP in download build; do
 	for TARG in ${TARGETS}; do
